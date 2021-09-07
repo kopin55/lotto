952c950c2b129284bcdd9c251b36fa8d6b815d9d
kopin55 <54444452+kopin55@users.noreply.github.com>
2021-09-08 06:36:25 +0900

    Add files via upload

---
+++ lotto.py
@@ -0,0 +1,10 @@
+import random
+print("이번엔 될거야")
+a = int(input())
+
+for i in range(a):
+    #lotto = random.sample(range(1,7),6)
+    #print(lotto)
+    lotto = random.sample(range(1,46),6)
+    print(lotto)
+    
